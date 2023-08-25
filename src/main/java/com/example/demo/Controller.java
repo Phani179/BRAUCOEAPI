fe9f299780f18b9ac30749062efc71ea5445a93d
= <=>
2023-08-25 20:23:38 +0530

    Updated API

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Entity.Student;
+import com.example.demo.Entity.StudentReg_No;
 import com.example.demo.Service.StudentsData;
 
 @RestController
@@ -18,9 +22,11 @@ public class Controller
 	StudentsData studentsData;
 	
 	@GetMapping("/student/{reg_no}")
-	public Optional<Student> getSpecificUser(@PathVariable int reg_no) 
+	public List<Optional<Student>> getSpecificUser(@PathVariable int reg_no) 
 	{
-		return studentsData.getStudentData(reg_no);
+		List<Optional<Student>> student = new ArrayList();
+		student.add(studentsData.getStudentData(reg_no));
+		return student;
 	}
 	
 	@GetMapping("/students")
