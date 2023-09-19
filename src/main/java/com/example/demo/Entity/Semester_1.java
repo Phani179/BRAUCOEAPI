9ff244cdacbbffe39d211312803fd148265fd596
= <=>
2023-09-20 00:40:31 +0530

    Semester 1 Results API Modified

--- src/main/java/com/example/demo/Entity/Semester_1.java
+++ src/main/java/com/example/demo/Entity/Semester_1.java
@@ -19,60 +19,41 @@ public class Semester_1
 	@Column(name = "Registration_no")
 	private long student_id;
 	
-	private int pap1c;
-	private String pap1n;
-	private float pap1gr;
-		
-	private int pap2c;
-	private String pap2n;
-	private float pap2gr;
-
-	private int pap3c;
-	private String pap3n;
-	private float pap3gr;
-
-	private int pap4c;
-	private String pap4n;
-	private float pap4gr;
-	
-	private int pap5c;
-	private String pap5n;
-	private float pap5gr;
-	
-	private int pap6c;
-	private String pap6n;
-	private float pap6gr;
-
-	public int getPap6c() {
-		return pap6c;
-	}
-	public void setPap6c(int pap6c) {
-		this.pap6c = pap6c;
-	}
-	public String getPap6n() {
-		return pap6n;
-	}
-	public void setPap6n(String pap6n) {
-		this.pap6n = pap6n;
-	}
-	public float getPap6gr() {
-		return pap6gr;
-	}
-	public void setPap6gr(float pap6gr) {
-		this.pap6gr = pap6gr;
-	}
-	
+    private float sgpa;
+    private String pap1n;
+    private String pap1gr;
+    private float pap1grp;
+    
+    private String pap2n;
+    private String pap2gr;
+    private float pap2grp;
+    
+    private String pap3n;
+    private String pap3gr;
+    private float pap3grp;
+    
+    private String pap4n;
+    private String pap4gr;
+    private float pap4grp;
+    
+    private String pap5n;
+    private String pap5gr;
+    private float pap5grp;
+    
+    private String pap6n;
+    private String pap6gr;
+    private float pap6grp;
 	public long getStudent_id() {
 		return student_id;
 	}
 	public void setStudent_id(long student_id) {
 		this.student_id = student_id;
 	}
-	public int getPap1c() {
-		return pap1c;
+	public float getSgpa() {
+		return sgpa;
 	}
-	public void setPap1c(int pap1c) {
-		this.pap1c = pap1c;
+	public void setSgpa(float sgpa) {
+		this.sgpa = sgpa;
 	}
 	public String getPap1n() {
 		return pap1n;
@@ -80,17 +61,17 @@ public class Semester_1
 	public void setPap1n(String pap1n) {
 		this.pap1n = pap1n;
 	}
-	public float getPap1gr() {
+	public String getPap1gr() {
 		return pap1gr;
 	}
-	public void setPap1gr(float pap1gr) {
+	public void setPap1gr(String pap1gr) {
 		this.pap1gr = pap1gr;
 	}
-	public int getPap2c() {
-		return pap2c;
+	public float getPap1grp() {
+		return pap1grp;
 	}
-	public void setPap2c(int pap2c) {
-		this.pap2c = pap2c;
+	public void setPap1grp(float pap1grp) {
+		this.pap1grp = pap1grp;
 	}
 	public String getPap2n() {
 		return pap2n;
@@ -98,17 +79,17 @@ public class Semester_1
 	public void setPap2n(String pap2n) {
 		this.pap2n = pap2n;
 	}
-	public float getPap2gr() {
+	public String getPap2gr() {
 		return pap2gr;
 	}
-	public void setPap2gr(float pap2gr) {
+	public void setPap2gr(String pap2gr) {
 		this.pap2gr = pap2gr;
 	}
-	public int getPap3c() {
-		return pap3c;
+	public float getPap2grp() {
+		return pap2grp;
 	}
-	public void setPap3c(int pap3c) {
-		this.pap3c = pap3c;
+	public void setPap2grp(float pap2grp) {
+		this.pap2grp = pap2grp;
 	}
 	public String getPap3n() {
 		return pap3n;
@@ -116,17 +97,17 @@ public class Semester_1
 	public void setPap3n(String pap3n) {
 		this.pap3n = pap3n;
 	}
-	public float getPap3gr() {
+	public String getPap3gr() {
 		return pap3gr;
 	}
-	public void setPap3gr(float pap3gr) {
+	public void setPap3gr(String pap3gr) {
 		this.pap3gr = pap3gr;
 	}
-	public int getPap4c() {
-		return pap4c;
+	public float getPap3grp() {
+		return pap3grp;
 	}
-	public void setPap4c(int pap4c) {
-		this.pap4c = pap4c;
+	public void setPap3grp(float pap3grp) {
+		this.pap3grp = pap3grp;
 	}
 	public String getPap4n() {
 		return pap4n;
@@ -134,17 +115,17 @@ public class Semester_1
 	public void setPap4n(String pap4n) {
 		this.pap4n = pap4n;
 	}
-	public float getPap4gr() {
+	public String getPap4gr() {
 		return pap4gr;
 	}
-	public void setPap4gr(float pap4gr) {
+	public void setPap4gr(String pap4gr) {
 		this.pap4gr = pap4gr;
 	}
-	public int getPap5c() {
-		return pap5c;
+	public float getPap4grp() {
+		return pap4grp;
 	}
-	public void setPap5c(int pap5c) {
-		this.pap5c = pap5c;
+	public void setPap4grp(float pap4grp) {
+		this.pap4grp = pap4grp;
 	}
 	public String getPap5n() {
 		return pap5n;
@@ -152,10 +133,34 @@ public class Semester_1
 	public void setPap5n(String pap5n) {
 		this.pap5n = pap5n;
 	}
-	public float getPap5gr() {
+	public String getPap5gr() {
 		return pap5gr;
 	}
-	public void setPap5gr(float pap5gr) {
+	public void setPap5gr(String pap5gr) {
 		this.pap5gr = pap5gr;
+	}
+	public float getPap5grp() {
+		return pap5grp;
+	}
+	public void setPap5grp(float pap5grp) {
+		this.pap5grp = pap5grp;
+	}
+	public String getPap6n() {
+		return pap6n;
+	}
+	public void setPap6n(String pap6n) {
+		this.pap6n = pap6n;
+	}
+	public String getPap6gr() {
+		return pap6gr;
+	}
+	public void setPap6gr(String pap6gr) {
+		this.pap6gr = pap6gr;
+	}
+	public float getPap6grp() {
+		return pap6grp;
+	}
+	public void setPap6grp(float pap6grp) {
+		this.pap6grp = pap6grp;
 	}
 }
